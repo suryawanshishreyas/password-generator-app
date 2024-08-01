23f425ea534c7ef171a8b728ae19bffec037edbb
Shreyas Suryawanshi <64725429+suryawanshishreyas@users.noreply.github.com>
2024-08-01 23:20:46 +0530

    Some fix needs to be made

---
+++ index.js
@@ -0,0 +1,25 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
+    "N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d",
+    "e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u",
+    "v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=",
+    "{","[","}","]",",","|",":",";","<",">",".","?","/"
+];
+
+let button = document.querySelector("#btn");
+let passwordGenerated = document.getElementById("password-space");
+let password = "";
+
+button.addEventListener("click",function (){
+    let j = 18;
+    for(i=0; i<characters.length; i++){
+        let randomNo = Math.floor(Math.random()* 18);
+        let randomChar = characters[randomNo];
+        password += randomChar;
+        password = password.substring(0,j);
+        console.log(password);
+        passwordGenerated.textContent = password;
+    }
+    return password = ""
+})
+
